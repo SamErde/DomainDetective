f998763343e3efc0eec59d285a9dd8e23a08b6ba
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-16 20:56:05 +0200

    fix(tests): 🐛 prevent port collisions

--- DomainDetective.Tests/PortHelper.cs
+++ DomainDetective.Tests/PortHelper.cs
@@ -3,18 +3,20 @@ namespace DomainDetective.Tests;
 using System.Net;
 using System.Net.Sockets;
 
-internal static class PortHelper
-{
+internal static class PortHelper {
     private static readonly object PortLock = new();
+    private static readonly HashSet<int> UsedPorts = new();
 
-    public static int GetFreePort()
-    {
-        lock (PortLock)
-        {
-            var listener = new TcpListener(IPAddress.Loopback, 0);
-            listener.Start();
-            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
-            listener.Stop();
+    public static int GetFreePort() {
+        lock (PortLock) {
+            int port;
+            do {
+                var listener = new TcpListener(IPAddress.Loopback, 0);
+                listener.Start();
+                port = ((IPEndPoint)listener.LocalEndpoint).Port;
+                listener.Stop();
+            } while (!UsedPorts.Add(port));
+
             return port;
         }
     }
