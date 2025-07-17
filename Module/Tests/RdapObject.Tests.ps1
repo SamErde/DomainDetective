35b7fcef37e02eff780301464bcb8cdb9f4984ed
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-17 13:35:20 +0200

    fix(tests): 🐛 allow offline rdap queries

--- Module/Tests/RdapObject.Tests.ps1
+++ Module/Tests/RdapObject.Tests.ps1
@@ -1,12 +1,14 @@
 Describe 'Get-RdapObject cmdlet' {
     It 'retrieves domain data' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        $result = Get-RdapObject -Domain 'example.com'
+        $data = Join-Path $PSScriptRoot 'Data'
+        $result = Get-RdapObject -Domain 'example.com' -ServiceEndpoint $data
         $result | Should -Not -BeNullOrEmpty
     }
     It 'retrieves IP data' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        $result = Get-RdapObject -Ip '192.0.2.1'
+        $data = Join-Path $PSScriptRoot 'Data'
+        $result = Get-RdapObject -Ip '192.0.2.1' -ServiceEndpoint $data
         $result | Should -Not -BeNullOrEmpty
     }
 }
