a8ca81ddbff98ff1c4af7bbbaab76d6b2a5a8624
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-18 08:03:49 +0200

    test(spf): 🧪 avoid network in summary test

--- DomainDetective.Tests/TestDomainSummary.cs
+++ DomainDetective.Tests/TestDomainSummary.cs
@@ -36,6 +36,7 @@ namespace DomainDetective.Tests {
             const string dkimRecord = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;";
 
             var healthCheck = new DomainHealthCheck();
+            healthCheck.SpfAnalysis.TestSpfRecords["_spf.google.com"] = "v=spf1 -all";
             await healthCheck.CheckSPF(spfRecord);
             await healthCheck.CheckDMARC(dmarcRecord);
             await healthCheck.CheckDKIM(dkimRecord);
