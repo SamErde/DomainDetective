0e145cb176cc71effdeb4472f5772f9e4dd2f13c
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-24 20:51:14 +0200

    test(cli): ✅ verify autodiscover endpoint settings

--- Module/Tests/Autodiscover.Tests.ps1
+++ Module/Tests/Autodiscover.Tests.ps1
@@ -1,7 +1,7 @@
 Describe 'Test-Autodiscover cmdlet' {
     It 'executes and returns data' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        $result = Test-Autodiscover -DomainName 'example.com' -DnsEndpoint System -IncludeEndpoints
+        $result = Test-Autodiscover -DomainName 'example.com' -DnsEndpoint System -IncludeEndpoints -ErrorAction SilentlyContinue
         $result | Should -Not -BeNullOrEmpty
         ($result | Measure-Object).Count | Should -BeGreaterThan 1
     }
