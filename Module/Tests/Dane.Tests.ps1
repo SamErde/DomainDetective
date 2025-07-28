f5f301d836df6566cdf73999e4b58fec42c83dd6
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-28 10:35:55 +0200

    fix(tests): 🐛 shorten DANE cancellation delays

--- Module/Tests/Dane.Tests.ps1
+++ Module/Tests/Dane.Tests.ps1
@@ -5,7 +5,7 @@ Describe 'Test-DaneRecord cmdlet' {
             Import-Module "$using:PSScriptRoot/../DomainDetective.psd1" -Force
             Test-TlsDane -DomainName 'does-not-exist.invalid' -DnsEndpoint System -Verbose
         }
-        Start-Sleep -Milliseconds 50
+        Start-Sleep -Milliseconds 20
         Stop-Job $job
         Wait-Job $job
         $job.ChildJobs[0].State | Should -Be 'Stopped'
@@ -16,7 +16,7 @@ Describe 'Test-DaneRecord cmdlet' {
         $ps = [powershell]::Create()
         $ps.AddScript("Import-Module '$PSScriptRoot/../DomainDetective.psd1' -Force; Test-TlsDane -DomainName 'does-not-exist.invalid' -DnsEndpoint System -Verbose") | Out-Null
         $handle = $ps.BeginInvoke()
-        Start-Sleep -Milliseconds 200
+        Start-Sleep -Milliseconds 20
         $ps.Stop()
         $null = $handle.AsyncWaitHandle.WaitOne()
         $ps.InvocationStateInfo.State | Should -Be 'Stopped'
