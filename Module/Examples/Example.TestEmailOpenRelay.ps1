58b68d6c2ec2d7071a08479b18751ffcf9cb7e34
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-11 18:20:08 +0200

    fix(examples): 📝 restore example content

--- Module/Examples/Example.TestEmailOpenRelay.ps1
+++ Module/Examples/Example.TestEmailOpenRelay.ps1
@@ -1,12 +1,12 @@
-﻿# Clear-Host
+# Clear-Host
 
 Import-Module $PSScriptRoot\..\DomainDetective.psd1 -Force
 
-$Gmail = Test-EmailOpenRelay -HostName 'gmail-smtp-in.l.google.com' -Port 25 -Verbose -ErrorAction SilentlyContinue
+$Gmail = Test-EmailOpenRelay -HostName 'gmail-smtp-in.l.google.com' -Port 25 -Verbose
 $Gmail | Format-Table
 
-$Example = Test-EmailOpenRelay -HostName 'gmail-smtp-in.l.google.com' -Port 25 -Verbose -ErrorAction SilentlyContinue
+$Example = Test-EmailOpenRelay -HostName 'mail.example.com' -Port 25 -Verbose -ErrorAction SilentlyContinue
 $Example | Format-Table
 
-$Example = Test-EmailOpenRelay -HostName 'gmail-smtp-in.l.google.com' -ErrorAction SilentlyContinue
+$Example = Test-EmailOpenRelay -HostName 'mail.example.com' -ErrorAction SilentlyContinue
 $Example | Format-Table
