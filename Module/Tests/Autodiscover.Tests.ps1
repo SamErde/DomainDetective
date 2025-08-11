4326b1efffbb379ec18dbedbdf9c1b4d0f8bb5a2
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-11 13:06:46 +0200

    fix(powershell): 🐛 align AutoDiscover cmdlet naming

--- Module/Tests/Autodiscover.Tests.ps1
+++ Module/Tests/Autodiscover.Tests.ps1
@@ -1,12 +1,12 @@
-Describe 'Test-Autodiscover cmdlet' {
+Describe 'Test-EmailAutoDiscover alias' {
     It 'executes and returns data' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        $result = Test-Autodiscover -DomainName 'example.com' -DnsEndpoint System -IncludeEndpoints -ErrorAction SilentlyContinue
+        $result = Test-EmailAutoDiscover -DomainName 'example.com' -DnsEndpoint System -IncludeEndpoints -ErrorAction SilentlyContinue
         $result | Should -Not -BeNullOrEmpty
         ($result | Measure-Object).Count | Should -BeGreaterThan 1
     }
     It 'throws if DomainName is empty' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        { Test-Autodiscover -DomainName '' } | Should -Throw
-    }
+        { Test-EmailAutoDiscover -DomainName '' } | Should -Throw
+}
 }
